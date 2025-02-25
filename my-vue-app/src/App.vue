dd872769a87c5381ab1f55b367a4f1b5cb5bd4fb
cmorenosu <cmorenosu@unal.edu.co>
2025-02-25 07:34:32 +0000

    feat: se crea el menu y se acualiza el codigo fuiente

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import MenuPrincipal from './components/Menu.vue'
+</script>
+
+<template>
+  <div class="container-fluid">
+    <MenuPrincipal/>
+    <HelloWorld />
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ my-vue-app/src/components/Menu.vue
@@ -0,0 +1,50 @@
+<!--este seccion de los templates sirve para todo el codigo html-->
+<template>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            
+            <a class="navbar-brand" href="https://www.google.com/">nombre del proyecto</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">inicio</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">contactos</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">productos</a>
+                    </li>                    
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+
+
+</template>
+
+
+
+<!--esta seccion sirve para la logica y es neto js-->
+<script>
+
+</script>
+
+
+
+
+<!--este seccion se encarga del diseño si se coloca scope solo servira en el componente-->
+<style scope></style>
